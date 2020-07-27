bd0acebac88de206d151115270e389828e1cf388
Felipe Reckziegel <felipesreck@gmail.com>
2020-07-27 20:37:49 -0300

    Added heading and "Binary Input" label

---
+++ src/components/Converter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Converter.css";
+
+const Converter = () => {
+  return (
+    <>
+      <h1>Binary to Decimal Converter</h1>
+      <div className="StyledForm">
+        <div className="Field">
+          <div className="Label">Binary Input</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Converter;
