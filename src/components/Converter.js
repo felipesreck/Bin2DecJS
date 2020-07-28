47b8f0e7f062b0f352e13c37c83e8f05fa50b73f
Felipe Reckziegel <felipesreck@gmail.com>
2020-07-28 12:33:56 -0300

    Methods and CSS added

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,13 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Converter.css";
 
 const Converter = () => {
+  const [binaryText, setBinaryText] = useState("");
+  const [decimalText, setDecimalText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onFormSubmit = () => {
+    const checkOnlyZerosAndOnes = () => {
+      if (binaryText.match(/^[0-1]+$/g) === null) {
+        setErrorMessage("Enter either 0 or 1");
+        return false;
+      } else {
+        return true;
+      }
+    };
+
+    const clearErrorMessage = () => {
+      setErrorMessage("");
+    };
+
+    const convertAndShowBinaryToDecimal = () => {
+      const reversedBinaryText = binaryText.split("").map(Number).reverse();
+      const result = reversedBinaryText.reduce(
+        (previousValue, currentValue, currentIndex) => {
+          return previousValue + currentValue * Math.pow(2, currentIndex);
+        }
+      );
+      setDecimalText(result);
+    };
+
+    if (!checkOnlyZerosAndOnes()) {
+      return;
+    }
+    clearErrorMessage();
+    convertAndShowBinaryToDecimal();
+  };
+
   return (
     <>
-      <h1>Binary to Decimal Converter</h1>
-      <div className="StyledForm">
+      <div className="BackgroundContainer">
+        <h1>Binary to Decimal Converter</h1>
+        <span id="Error">{errorMessage}</span>
+
         <div className="Field">
-          <div className="Label">Binary Input</div>
+          <div>
+            <div id="Label">Binary Input</div>
+            <input
+              id="TextInput"
+              type="text"
+              name="binary"
+              placeholder="Enter 0 or 1"
+              value={binaryText}
+              onChange={(event) => setBinaryText(event.target.value)}
+            />
+          </div>
+          <div>
+            <div id="Label">Decimal Output</div>
+            <input
+              id="TextInput"
+              type="text"
+              name="decimal"
+              value={decimalText}
+              disabled
+            />
+          </div>
+          <button id="ConvertButton" type="submit" onClick={onFormSubmit}>
+            Convert
+          </button>
         </div>
       </div>
     </>
